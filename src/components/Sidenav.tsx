c2d98eb2a7a94f3e7fa0f21dbe4de9be4e9240c8
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-14 18:27:42 -0700

    Fix padding issue, void signIn/signOut result

--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -20,11 +20,11 @@ export default function Sidenav() {
 
         {user == null ? (
           <li>
-            <button onClick={() => signIn()}>Log In</button>
+            <button onClick={() => void signIn()}>Log In</button>
           </li>
         ) : (
           <li>
-            <button onClick={() => signOut()}>Log Out</button>
+            <button onClick={() => void signOut()}>Log Out</button>
           </li>
         )}
       </ul>
