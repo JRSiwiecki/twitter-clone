c7e710685533eae542adc0404cff2d7c32233acf
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-14 18:17:11 -0700

    Add Home and User links, add sidenav

---
+++ src/components/Sidenav.tsx
@@ -0,0 +1,23 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+export default function Sidenav() {
+  const session = useSession();
+  const user = session.data?.user;
+
+  return (
+    <nav className="sticky top-0 px-2 py-4">
+      <ul className="flex flex-col items-start gap-2 whitespace-nowrap">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+
+        {user != null && (
+          <li>
+            <Link href={`/profiles/${user.id}`}>Profile</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
