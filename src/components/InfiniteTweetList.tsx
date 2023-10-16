c12da1f9701eb67ce6631fbef9b46e05463736f2
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-15 20:18:03 -0700

    Add heart icon to tweets

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import InfiniteScroll from "react-infinite-scroll-component";
 import ProfileImage from "./ProfileImage";
+import { useSession } from "next-auth/react";
+import { VscHeart, VscHeartFilled } from "react-icons/vsc";
 
 type Tweet = {
   id: string;
@@ -85,12 +87,29 @@ function TweetCard({
           </span>
         </div>
         <p className="whitespace-pre-wrap">{content}</p>
-        <HeartButton />
+        <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
       </div>
     </li>
   );
 }
 
-function HeartButton() {
-  return <h1>Heart</h1>;
+type HeartButtonProps = {
+  likedByMe: boolean;
+  likeCount: number;
+};
+
+function HeartButton({ likedByMe, likeCount }: HeartButtonProps) {
+  const session = useSession();
+  const HeartIcon = likedByMe ? VscHeartFilled : VscHeart;
+
+  if (session.status !== "authenticated") {
+    return (
+      <div className="mb-1 mt-1 flex items-center gap-3 self-start text-gray-500">
+        <HeartIcon />
+        <span>{likeCount}</span>
+      </div>
+    );
+  }
+
+  return <h1>♥</h1>;
 }
