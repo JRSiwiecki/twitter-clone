b0ac52326679dc60798b5d0a2890b61e9f3f8452
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-15 19:53:04 -0700

    Add infinite tweet router, update schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 import NewTweetForm from "~/components/NewTweetForm";
 
 export default function Home() {
@@ -7,6 +8,13 @@ export default function Home() {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewTweetForm />
+      <RecentTweets />
     </>
   );
+}
+
+function RecentTweets() {
+  const tweets = [];
+
+  return <InfiniteTweetList tweets={tweets} />;
 }
