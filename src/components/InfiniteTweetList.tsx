af709fbe1c93731665fd1da3f98a5b8c0b712c7e
JRSiwiecki <JRSiwiecki@gmail.com>
2023-10-15 20:14:24 -0700

    Style infiniteTweet list more

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import InfiniteScroll from "react-infinite-scroll-component";
+import ProfileImage from "./ProfileImage";
 
 type Tweet = {
   id: string;
@@ -45,9 +47,50 @@ export function InfiniteTweetList({
         loader={"Loading..."}
       >
         {tweets.map((tweet) => {
-          return <div key={tweet.id}>{tweet.content}</div>;
+          return <TweetCard key={tweet.id} {...tweet} />;
         })}
       </InfiniteScroll>
     </ul>
   );
+}
+
+const dateTimeFormatter = new Intl.DateTimeFormat(undefined, {
+  dateStyle: "short",
+});
+
+function TweetCard({
+  id,
+  user,
+  content,
+  createdAt,
+  likeCount,
+  likedByMe,
+}: Tweet) {
+  return (
+    <li className="flex gap-4 border-b px-4 py-4">
+      <Link href={`/profiles/${user.id}`}>
+        <ProfileImage src={user.image} />
+      </Link>
+      <div className="flex flex-grow flex-col">
+        <div className="flex gap-1">
+          <Link
+            className="font-bold outline-none hover:underline focus-visible:underline"
+            href={`/profiles/${user.id}`}
+          >
+            {user.name}
+          </Link>
+          <span className="text-gray-500">-</span>
+          <span className="text-gray-500">
+            {dateTimeFormatter.format(createdAt)}
+          </span>
+        </div>
+        <p className="whitespace-pre-wrap">{content}</p>
+        <HeartButton />
+      </div>
+    </li>
+  );
+}
+
+function HeartButton() {
+  return <h1>Heart</h1>;
 }
