29c25e1dd61590753fdf44607c4bb446493ee487
JRSiwiecki <JRSiwiecki@gmail.com>
2023-11-12 13:01:29 -0800

    Add update for when new tweet is posted, show likes without refresh

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -36,6 +36,8 @@ function Form() {
     textAreaReference.current = textArea;
   }, []);
 
+  const trpcUtils = api.useContext();
+
   useLayoutEffect(() => {
     updateTextAreaSize(textAreaReference.current);
   }, [inputValue]);
@@ -43,6 +45,34 @@ function Form() {
   const createTweet = api.tweet.create.useMutation({
     onSuccess: (newTweet) => {
       setInputValue("");
+
+      if (session.status !== "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData == null || oldData.pages[0] == null) return;
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name || null,
+            image: session.data.user.image || null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
     },
   });
 
