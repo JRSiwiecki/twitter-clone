b987f89bb5cb8b0d1119a17b6c044c05aedac977
JRSiwiecki <JRSiwiecki@gmail.com>
2023-11-13 18:46:51 -0800

    Add revalidation logic for profile page

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -63,6 +63,8 @@ export const profileRouter = createTRPCRouter({
       }
 
       // Revalidation
+      void ctx.revalidateSSG?.(`/profiles/${userId}`);
+      void ctx.revalidateSSG?.(`/profiles/${currentUserId}`);
 
       return { addedFollow };
     }),
